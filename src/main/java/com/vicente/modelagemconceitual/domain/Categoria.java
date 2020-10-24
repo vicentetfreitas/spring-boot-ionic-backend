d54f63d2d0837fdb02388dcea8788d67b41fce5d
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-10-24 16:26:46 -0300

    Banco de dados H2 e criação automatica da base de dados.

--- src/main/java/com/vicente/modelagemconceitual/domain/Categoria.java
+++ src/main/java/com/vicente/modelagemconceitual/domain/Categoria.java
@@ -2,14 +2,22 @@ package com.vicente.modelagemconceitual.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	// Atributos
+	// ATRIBUTOS
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
-	// Contrutores
+	// CONSTRUTORES
 	public Categoria() {
 	}
 
