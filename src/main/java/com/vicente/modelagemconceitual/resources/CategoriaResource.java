f20a5e9ffa2a4efa5b7414b5c5dd782c7d1ef144
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-10-24 15:55:19 -0300

    Testando a primeira classe de domínio.

--- src/main/java/com/vicente/modelagemconceitual/resources/CategoriaResource.java
+++ src/main/java/com/vicente/modelagemconceitual/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.vicente.modelagemconceitual.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vicente.modelagemconceitual.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String Listar() {
-		return ("REST está funcionando.");
+	public List<Categoria> Listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();	
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
