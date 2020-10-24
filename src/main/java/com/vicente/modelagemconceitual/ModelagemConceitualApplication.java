a3d531148da394939b5a946a92afd173cb3bb9e8
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-10-24 18:24:02 -0300

    Criando operação de instanciação.

--- src/main/java/com/vicente/modelagemconceitual/ModelagemConceitualApplication.java
+++ src/main/java/com/vicente/modelagemconceitual/ModelagemConceitualApplication.java
@@ -1,13 +1,32 @@
 package com.vicente.modelagemconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vicente.modelagemconceitual.domain.Categoria;
+import com.vicente.modelagemconceitual.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ModelagemConceitualApplication {
+public class ModelagemConceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
