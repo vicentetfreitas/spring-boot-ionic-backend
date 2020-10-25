47ea5ff9d609df41f1ec8b3eb0e8da20cab8cb44
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-10-25 10:16:31 -0300

    Endpoint /cliente{id} disponivel.

--- src/main/java/com/vicente/modelagemconceitual/repositories/EnderecoRepository.java
+++ src/main/java/com/vicente/modelagemconceitual/repositories/EnderecoRepository.java
@@ -1,9 +1,11 @@
 package com.vicente.modelagemconceitual.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.vicente.modelagemconceitual.domain.Endereco;
 
+@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
 
 }
