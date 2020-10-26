e8d0962291aa1aac89008fa6628d9b144bdbf0f0
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-10-25 21:36:42 -0300

    ItemPedido e ItemPedidoPK.

--- src/main/java/com/vicente/modelagemconceitual/ModelagemConceitualApplication.java
+++ src/main/java/com/vicente/modelagemconceitual/ModelagemConceitualApplication.java
@@ -13,6 +13,7 @@ import com.vicente.modelagemconceitual.domain.Cidade;
 import com.vicente.modelagemconceitual.domain.Cliente;
 import com.vicente.modelagemconceitual.domain.Endereco;
 import com.vicente.modelagemconceitual.domain.Estado;
+import com.vicente.modelagemconceitual.domain.ItemPedido;
 import com.vicente.modelagemconceitual.domain.Pagamento;
 import com.vicente.modelagemconceitual.domain.PagamentoComBoleto;
 import com.vicente.modelagemconceitual.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.vicente.modelagemconceitual.repositories.CidadeRepository;
 import com.vicente.modelagemconceitual.repositories.ClienteRepository;
 import com.vicente.modelagemconceitual.repositories.EnderecoRepository;
 import com.vicente.modelagemconceitual.repositories.EstadoRepository;
+import com.vicente.modelagemconceitual.repositories.ItemPedidoRepository;
 import com.vicente.modelagemconceitual.repositories.PagamentoRepository;
 import com.vicente.modelagemconceitual.repositories.PedidoRepository;
 import com.vicente.modelagemconceitual.repositories.ProdutoRepository;
@@ -49,6 +51,8 @@ public class ModelagemConceitualApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
@@ -133,6 +137,20 @@ public class ModelagemConceitualApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
+		//////////////////////////////////////////////////////////////
+
+		ItemPedido ip1 = new ItemPedido(ped1, prod1, 0.00, 1, 2000.0);
+		ItemPedido ip2 = new ItemPedido(ped1, prod3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, prod2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		prod1.getItens().addAll(Arrays.asList(ip1));
+		prod2.getItens().addAll(Arrays.asList(ip3));
+		prod3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
---
+++ src/main/java/com/vicente/modelagemconceitual/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.vicente.modelagemconceitual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vicente.modelagemconceitual.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
