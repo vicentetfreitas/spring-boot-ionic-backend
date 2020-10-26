f7d26843add7369f05dd97c10ab67d2d99e4163a
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-10-26 09:27:24 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/vicente/modelagemconceitual/services/CategoriaService.java
+++ src/main/java/com/vicente/modelagemconceitual/services/CategoriaService.java
@@ -18,6 +18,11 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
